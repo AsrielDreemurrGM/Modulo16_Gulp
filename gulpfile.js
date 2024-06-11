ed451661fdd41f8a5a3491e780f26d7b63993c45
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-06-11 00:53:34 -0300

    Executado Função compilaSass
    - Alterado Para ao Invés de Compilar Todos os Arquivos .scss Compilar  Somente o main.scss;

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function compilaSass() {
-    return gulp.src('./source/styles/*.scss')
+    return gulp.src('./source/styles/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('./build/styles'));
 }
