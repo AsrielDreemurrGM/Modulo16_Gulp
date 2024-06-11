94b5987fe17fa8c8c4c219945ada645fddc55c4b
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-06-11 01:01:28 -0300

    Instalado Plugin SourceMaps do Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
