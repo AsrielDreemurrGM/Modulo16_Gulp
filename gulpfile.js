74701c079d2c7ab53d8eb9fb25e5cf082ba68760
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-06-11 23:57:19 -0300

    Iniciado Organização do Projeto
    - Removido Códigos Desnecessários;

--- gulpfile.js
+++ gulpfile.js
@@ -28,28 +28,6 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao(callback) {
-    setTimeout(function() {
-        console.log("Executando via Gulp");
-        callback();
-    }, 2000);
-}
-
-function dizOi(callback) {
-    console.log("Olá Gulp");
-    dizTchau();
-    callback();
-}
-
-function dizTchau() {
-    console.log("tchau Gulp");
-}
-
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
-exports.watch = function() {
+exports.default = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
 }
-exports.javascript = comprimeJavaScript;
-exports.images = comprimeImagens;
